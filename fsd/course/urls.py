40a04ee762901285ba8d78b441cc6b66ce6b7e2d
nandinig2001 <gawankarnandini@gmail.com>
2021-09-22 15:17:03 +0530

    index base setup

---
+++ fsd/course/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register',views.registerPage,name='register'),
+    path('login',views.loginPage,name='login'),
+    path('logout',views.logoutPage,name='logout'),
+
+]
